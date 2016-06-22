8a5112de8c637c2fda7da6df2a5a81c5f821fdcf
Thun Pin Chiu <thunpin@gmail.com>
2016-06-22 16:25:24 -0300

    [na]

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ var routers = require('./routers');
 app = express();
 routers(app);
 
-app.listen(4000, function() {
-    tools.logTitle('zwave-web REST listening on port 4000!');
+app.listen(5000, function() {
+    tools.logTitle('zwave-web REST listening on port 5000!');
 });
